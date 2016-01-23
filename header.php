b4163117cf950e43bb5bf43c23c84c3bac7f3587
Eric Defore <eric.defore@gmail.com>
2016-01-23 17:16:08 -0500

    Made Main Nav Stacked

--- header.php
+++ header.php
@@ -39,7 +39,7 @@ if ( ! isset( $_SESSION ) ) {
         <div id="wrapper" class = "off-canvass-wrapper-inner row" data-off-canvas-wrapper>
 
             <div class="off-canvas position-left nav-menu" id="offCanvasLeft" data-off-canvas>
-            
+
                 <?php
                 wp_nav_menu( array(
                     'container' => false,
@@ -51,14 +51,14 @@ if ( ! isset( $_SESSION ) ) {
                     'walker' => new Foundation_Nav_Walker(),
                 ) );
                 ?>
-            
+
             </div>
 
             <div class="off-canvas-content" data-off-canvas-content>
 
                 <header id="site-header">
 
-                    <div class="top-bar">
+                    <div class="top-bar small-12 medium-3 columns">
 
                         <div class="top-bar-left hide-for-small-only nav-menu">
                             <?php
@@ -67,7 +67,7 @@ if ( ! isset( $_SESSION ) ) {
                                 'menu' => __( 'Primary Menu', THEME_ID ),
                                 'menu_class' => 'dropdown menu',
                                 'theme_location' => 'primary-nav',
-                                'items_wrap'      => '<ul id="%1$s" class="%2$s" data-dropdown-menu>%3$s</ul>',
+                                'items_wrap'      => '<ul id="%1$s" class="vertical %2$s" data-dropdown-menu>%3$s</ul>',
                                 'fallback_cb' => false,
                                 'walker' => new Foundation_Nav_Walker(),
                             ) );
@@ -81,8 +81,28 @@ if ( ! isset( $_SESSION ) ) {
                         </div>
 
                     </div>
+
+                    <?php
+                    // We need to assign the height of the image based on the number of Top-Level Menu Items. It is a nasty solution, but it works.
+                    // Otherwise, we'd need to calculate it via JavaScript. In some ways that's better, but it could make the page "jump" as it loads.
+                    $menu_to_count = wp_nav_menu( array(
+                        'echo' => false,
+                        'theme_location' => 'primary-nav',
+                        'depth' => 1,
+                    ) );
+                    $menu_items = substr_count( $menu_to_count, 'class="menu-item ' );
+                    $menu_item_height = 39.375;
+                    $padding_top_plus_bottom = 16;
                     
-                    <img class="header-logo" src="<?php echo get_theme_mod( 'wasentha_logo_image', 'http://placehold.it/1200x312' ); ?>" />
+                    $image_height = ( $menu_item_height * $menu_items ) + 16;
+                    $image_height = $image_height . 'px';
+                    ?>
+
+
+                    <div class="header-logo small-12 medium-9 columns hide-for-small-only" style="background-image: url('<?php echo get_theme_mod( 'wasentha_logo_image', 'http://placehold.it/1200x312' ); ?>'); height: <?php echo $image_height; ?>;">
+                    </div>
+                    
+                    <img class="header-logo-mobile show-for-small-only small-12" src="<?php echo get_theme_mod( 'wasentha_logo_image', 'http://placehold.it/1200x312' ); ?>" />
 
                 </header>
 
